3da76616fa814f10cd61cab94b892437016f2c18
diegoRamiirez <dieego.rz@hotmail.com>
2024-10-13 19:35:40 +0200

    HiperLink

---
+++ hiperLink.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("http://www.w3schools.com/")
+time.sleep(5)
+
+encontrar_link = driver.find_element(By.LINK_TEXT, 'Learn PHP')
+encontrar_link.click()
+
+# Recuerda cerrar el navegador al final
+driver.quit()
