105ae273d52390cdd1f15c17173b575aac0480b8
diegoRamiirez <dieego.rz@hotmail.com>
2024-10-13 19:18:46 +0200

    displayed-elements

---
+++ displaed-elements.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.com")
+time.sleep(3)
+
+try:
+    displayelementRechazar = driver.find_element(By.NAME, "Rechazar todo")
+    displayelementRechazar.click()
+except:
+    print("No se encontró el botón de Rechazar todo")
+    
+time.sleep(3)
+
+try:
+    displayelement = driver.find_element(By.NAME, "btnI")
+    print(displayelement.is_enabled())  # Comprueba si el botón está habilitado
+    print(displayelement.is_displayed())  # Comprueba si el botón está visible
+except:
+    print("No se encontró el botón 'Voy a tener suerte'")
+
+driver.quit()
