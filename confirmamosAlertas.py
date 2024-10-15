99d7309db836634ead1cf7b7776460456f0af364
diegoRamiirez <dieego.rz@hotmail.com>
2024-10-15 18:28:41 +0200

    confirmacionAlerta

---
+++ confirmamosAlertas.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+
+file_path = "file:///Users/diegoramirez/TrabajosQA/alertSimple.html"
+driver.get(file_path)
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "alert")))
+alert_simple = driver.find_element(By.NAME, "confirmar_alert")
+alert_simple.click()
