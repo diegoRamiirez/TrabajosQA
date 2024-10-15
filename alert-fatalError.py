7f3bb7a235c952ac79f2f82604a1562e3e440234
diegoRamiirez <dieego.rz@hotmail.com>
2024-10-15 17:26:33 +0200

    CrearAlerta

---
+++ alert-fatalError.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+
+file_path = "file:///Users/diegoramirez/TrabajosQA/alertSimple.html"
+driver.get(file_path)
+time.sleep(5)
+driver.quit()
